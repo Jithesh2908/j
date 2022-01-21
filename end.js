ab84863bcfa9b1d8d4a71232e08ae58b54ad709c
Jithesh Hemakumar <69755088+Jithesh2908@users.noreply.github.com>
2022-01-21 13:34:37 +0530

    Add files via upload

---
+++ end.js
@@ -0,0 +1,18 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores =JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores)
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    console.log("click the save button!");
+    e.preventDefault();
+};
